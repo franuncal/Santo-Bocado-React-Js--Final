331de7ed5fddb85f790da38ff7d826262398706f
Francisco Uncal <franunca@gmail.com>
2024-02-09 17:54:41 -0300

    agregando base de firebase al proyecto

---
+++ src/Firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnyrXsUdmk3Sb2BxNwQ_NwFsFYpc2gO1s",
+  authDomain: "santo-bocado-55bf8.firebaseapp.com",
+  projectId: "santo-bocado-55bf8",
+  storageBucket: "santo-bocado-55bf8.appspot.com",
+  messagingSenderId: "983488600106",
+  appId: "1:983488600106:web:4d8100b046567d48b04b53",
+};
+
+const app = initializeApp(firebaseConfig);
